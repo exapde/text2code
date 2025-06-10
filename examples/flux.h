ada3728ad7aec6caeec959d9416376c3de4dc2eb
exapde <exapde@gmail.com>
2025-06-10 13:32:22 -0400

    delete h and cpp files in examples

--- examples/flux.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void flux(dstype* f, const dstype* uq, const dstype* w, const dstype* v, const dstype* x, const dstype time, const dstype* mu, const dstype* eta, const int N, const int szf, const int szuq, const int szw, const int szv, const int szx, const int szmu, const int szeta);
-void fluxjac(dstype* f, dstype* J1, dstype* J2, const dstype* uq, const dstype* w, const dstype* v, const dstype* x, const dstype time, const dstype* mu, const dstype* eta, const int N, const int szf, const int szuq, const int szw, const int szv, const int szx, const int szmu, const int szeta);
-void fluxjachess(dstype* f, dstype* J1, dstype* J2, dstype* H1, const dstype* uq, const dstype* w, const dstype* v, const dstype* x, const dstype time, const dstype* mu, const dstype* eta, const int N, const int szf, const int szuq, const int szw, const int szv, const int szx, const int szmu, const int szeta);
--- examples/initu.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void initu(dstype* f, const dstype* x, const int N, const int szf, const int szx);
